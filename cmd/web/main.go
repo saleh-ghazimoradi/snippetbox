3d8c3774b5c1f2cf11c5f0b17a25b60b8b6e76f2
Saleh Ghazimoradi <74179371+saleh-ghazimoradi@users.noreply.github.com>
2023-08-02 17:09:37 +0330

    add router to the porject

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	mux := http.NewServeMux()
+
+	mux.Handle("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Print("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
