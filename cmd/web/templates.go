9a5c099cb4143d3a10da49bb81fd24f57f7d476d
Saleh Ghazimoradi <74179371+saleh-ghazimoradi@users.noreply.github.com>
2023-08-03 15:29:37 +0330

    add render function to the project and improve newtemplate function

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -13,7 +13,6 @@ type templateData struct {
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
-
 	cache := map[string]*template.Template{}
 
 	pages, err := filepath.Glob("./ui/html/pages/*.html")
@@ -22,16 +21,19 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	}
 
 	for _, page := range pages {
-
 		name := filepath.Base(page)
 
-		files := []string{
-			"./ui/html/base.html",
-			"./ui/html/partials/nav.html",
-			page,
+		ts, err := template.ParseFiles("./ui/html/base.html")
+		if err != nil {
+			return nil, err
 		}
 
-		ts, err := template.ParseFiles(files...)
+		ts, err = ts.ParseGlob("./ui/html/partials/*.html")
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err = ts.ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
